49775c63c26348f1b3ec2504f0812edd4e185f3a
nickolanack <nickblackwell82@gmail.com>
2016-07-14 13:36:31 -0700

    add email

--- delayedEventHook.onCreateMapitem.php
+++ delayedEventHook.onCreateMapitem.php
@@ -9,6 +9,6 @@ $headers = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 $headers .= 'From: ' . 'Geolive'. ' <' .
  'admin@geolive.ca' . '>' . "\r\n";
-$to = array('nickblackwell82@gmail.com');
+$to = array('nickblackwell82@gmail.com', 'jon.corbett@gmail.com');
 $to = rtrim(implode(', ', $to), ',');
 mail($to, 'Submit to RAPP', 'Message', $headers);
