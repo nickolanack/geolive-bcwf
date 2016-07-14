5e9ca0d012e5425bb04212d732a290ed95e731fe
nickolanack <nickblackwell82@gmail.com>
2016-07-14 13:35:48 -0700

    added css for readaccess buttons (infowindow) added custom event on creating map items (delay then email - this is a test)

---
+++ delayedEventHook.onCreateMapitem.php
@@ -0,0 +1,14 @@
+<?php 
+
+sleep(180);
+
+Core::Emit('onDelayedCreateMapitem', $eventArgs);
+
+
+$headers = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= 'From: ' . 'Geolive'. ' <' .
+ 'admin@geolive.ca' . '>' . "\r\n";
+$to = array('nickblackwell82@gmail.com');
+$to = rtrim(implode(', ', $to), ',');
+mail($to, 'Submit to RAPP', 'Message', $headers);
