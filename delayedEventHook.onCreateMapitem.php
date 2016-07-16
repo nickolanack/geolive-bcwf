b5f9174e5ca9066241f25b5004773a14d277cfa1
nickolanack <nickblackwell82@gmail.com>
2016-07-16 13:15:46 -0700

    add data to email

--- delayedEventHook.onCreateMapitem.php
+++ delayedEventHook.onCreateMapitem.php
@@ -9,6 +9,18 @@ $headers = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 $headers .= 'From: ' . 'Geolive'. ' <' .
  'admin@geolive.ca' . '>' . "\r\n";
-$to = array('nickblackwell82@gmail.com', 'jon.corbett@gmail.com');
+$to = array('nickblackwell82@gmail.com');
 $to = rtrim(implode(', ', $to), ',');
-mail($to, 'Submit to RAPP', 'Message', $headers);
+
+Core::LoadPlugin('Maps');
+$marker=MapController::LoadMapItem($eventArgs->id);
+
+Core::LoadPlugin('Attributes');
+$attributes=AttributesRecord::Get($marker->getId(), $marker->getType(), "rappAttributes");
+
+mail($to, 'Submitted to RAPP', json_encode(array(
+	'eventArgs'=>$eventArgs,
+	'marker'=>$marker->getMetadata(),
+	'attributes'=>$attributes
+
+)), $headers);
