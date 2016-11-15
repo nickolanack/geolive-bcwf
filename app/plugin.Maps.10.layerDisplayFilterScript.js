7cb198572e9672efb2a6b45225a144c880b9e55e
nickolanack <apache@s54.ok.ubc.ca>
2016-11-15 14:09:03 -0800

    auto commit message

---
+++ app/plugin.Maps.10.layerDisplayFilterScript.js
@@ -0,0 +1,6 @@
+//throw new Exception(print_r($feature,true));
+
+if(Core::Client()->isAdmin()||Core::Client()->getUserId()===$feature['uid']){
+   return true;
+}
+return time()-strtotime($feature['creationDate'])>2*24*3600;
