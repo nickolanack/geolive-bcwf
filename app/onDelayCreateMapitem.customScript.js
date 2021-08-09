1afe6e4063cc9a22cb75f15e8e917c532ee2ab91
nickolanack <>
2021-08-09 16:11:21 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/onDelayCreateMapitem.customScript.js
+++ app/onDelayCreateMapitem.customScript.js
@@ -5,7 +5,14 @@
         $delay=max(0,min(intval($delay),10));
         sleep(max(10, 60 * $delay));
         
-        
+        try {
+            GetPlugin('Maps');
+			$marker = (new \spatial\FeatureLoader())->fromId($eventArgs->id);
+		} catch (Exception $e) {
+			Emit('onDelayedCreateMapitemMarkerWasDeletedWhileSleeping', array_merge(get_object_vars($eventArgs), array(
+				'error' => $e->getMessage(),
+			)));
+			
     
        // include_once GetPath('{front}/bcwf/BCWFSubmit.php');
        // BCWFSubmit::ProcessEvent($eventArgs);
