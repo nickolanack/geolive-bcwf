60b510988c96351cdc47bd557dd5ecb6167179d6
nickolanack <>
2021-08-09 16:08:15 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/onDelayCreateMapitem.customScript.js
+++ app/onDelayCreateMapitem.customScript.js
@@ -1,8 +1,13 @@
 <?php
 
+        $config=GetWidget('app-config');
+        $delay=$config->getParameter('delay', 4);
+        $delay=max(0,min(intval($delay),10));
+        sleep(max(10, 60 * $delay));
+        
         
     
-        include_once GetPath('{front}/bcwf/BCWFSubmit.php');
-        BCWFSubmit::ProcessEvent($eventArgs);
+       // include_once GetPath('{front}/bcwf/BCWFSubmit.php');
+       // BCWFSubmit::ProcessEvent($eventArgs);
        
 ?>
