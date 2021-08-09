c730a9e79f85a30f3f3a9a4d14aa6964e8692a21
nickolanack <>
2021-08-09 16:40:03 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/onCreateMapItem.customScript.js
+++ app/onCreateMapItem.customScript.js
@@ -5,7 +5,7 @@ $config=GetWidget('app-config');
 $delay=$config->getParameter('delay', 4);
 $delay=max(0,min(intval($delay),10));
 
-$eventArgs->config=$config->getConfigurationParameters();
+$eventArgs->config=$config->getConfigurationValues();
 Broadcast('onDelayCreateMapitem','event', $eventArgs);
 Emit('onDelayCreateMapitem', $eventArgs, $delay);
 
