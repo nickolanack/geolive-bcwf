85260af01a03eec49e1d7cd88527d4afe6b9d027
nickolanack <nickblackwell82@gmail.com>
2022-03-07 15:54:02 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

---
+++ app/quickStats.content.2.getDataScript.js
@@ -0,0 +1,8 @@
+callback({result:3060});
+
+
+(new AjaxControlQuery(CoreAjaxUrlRoot, "user_function", {
+		'widget': "mobileStats"
+	})).addEvent('success',function(response){
+	    
+	}).execute(); 
