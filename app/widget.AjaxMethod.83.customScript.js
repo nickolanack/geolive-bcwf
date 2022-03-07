98d96fac1c253e641693e6b358916405325d6b5e
nickolanack <nickblackwell82@gmail.com>
2022-03-07 15:53:08 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

---
+++ app/widget.AjaxMethod.83.customScript.js
@@ -0,0 +1,16 @@
+
+
+
+
+return array(
+    'ios'=>GetPlugin("Apps")->getDatabase()->countAccounts(array("username"=>array(
+					"comparator"=>"NOT LIKE",
+					"value"=>"android%"
+					)))
+
+    'android'=>GetPlugin("Apps")->getDatabase()->countAccounts(array("username"=>array(
+					"comparator"=>"LIKE",
+					"value"=>"android%"
+					)))
+    
+    );
